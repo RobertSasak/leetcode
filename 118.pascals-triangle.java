08a08621aa4f9920611bb5b2f70dabe49b9e0d58
Robert Sasak <robert.sasak@gmail.com>
2020-02-21 12:44:53 +0100

    Update 118.pascals-triangle.java

--- 118.pascals-triangle.java
+++ 118.pascals-triangle.java
@@ -7,7 +7,26 @@
 // @lc code=start
 class Solution {
     public List<List<Integer>> generate(int numRows) {
-        
+        LinkedList<List<Integer>> r = new LinkedList<>();
+        if (numRows == 0) {
+            return r;
+        }
+        LinkedList<Integer> list = new LinkedList<>();
+        list.add(1);
+        r.add(list);
+        while (numRows > 1) {
+            LinkedList<Integer> newList = new LinkedList<>();
+            int prev = 0;
+            for (int n : list) {
+                newList.add(n + prev);
+                prev = n;
+            }
+            newList.add(1);
+            r.add(newList);
+            list = newList;
+            numRows--;
+        }
+        return r;
     }
 }
 // @lc code=end
