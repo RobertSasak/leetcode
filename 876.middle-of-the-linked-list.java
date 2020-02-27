b62b101e1cecbc5671fb68cb43c3faf091eb5fa1
Robert Sasak <robert.sasak@gmail.com>
2020-02-27 19:55:12 +0100

    Create 876.middle-of-the-linked-list.java

---
+++ 876.middle-of-the-linked-list.java
@@ -0,0 +1,28 @@
+/*
+ * @lc app=leetcode id=876 lang=java
+ *
+ * [876] Middle of the Linked List
+ */
+
+// @lc code=start
+/**
+ * Definition for singly-linked list.
+ * public class ListNode {
+ *     int val;
+ *     ListNode next;
+ *     ListNode(int x) { val = x; }
+ * }
+ */
+class Solution {
+    public ListNode middleNode(ListNode head) {
+        ListNode fast = head;
+        ListNode slow = head;
+        while (fast != null && fast.next != null) {
+            fast = fast.next.next;
+            slow = slow.next;
+        }
+        return slow;
+    }
+}
+// @lc code=end
+
