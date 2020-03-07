8b85e6d5ab14238b7118ff5beea473891cf877a6
Robert Sasak <robert.sasak@gmail.com>
2020-03-07 17:26:00 +0100

    Create 1042.flower-planting-with-no-adjacent.java

---
+++ 1042.flower-planting-with-no-adjacent.java
@@ -0,0 +1,14 @@
+/*
+ * @lc app=leetcode id=1042 lang=java
+ *
+ * [1042] Flower Planting With No Adjacent
+ */
+
+// @lc code=start
+class Solution {
+    public int[] gardenNoAdj(int N, int[][] paths) {
+        
+    }
+}
+// @lc code=end
+
