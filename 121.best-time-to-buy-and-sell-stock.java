5f55c25a9615510fb58f54bbde028070ab755b8e
Robert Sasak <robert.sasak@gmail.com>
2020-04-13 16:03:56 +0200

    Update 121.best-time-to-buy-and-sell-stock.java

--- 121.best-time-to-buy-and-sell-stock.java
+++ 121.best-time-to-buy-and-sell-stock.java
@@ -8,6 +8,18 @@
 class Solution {
 
     public int maxProfit(int[] prices) {
+        if (prices.length == 0) return 0;
+        int min = prices[0];
+        int max = 0;
+        for (int i = 1; i < prices.length; i++) {
+            max = Math.max(max, prices[i] - min);
+            min = Math.min(min, prices[i]);
+        }
+        return max;
+    }
+}
+
+    public int maxProfitUgly(int[] prices) {
         int l = prices.length;
         if (l ==0) {
             return 0;
